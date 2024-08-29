3f09a9a404ace0905f52e9157a44f9322136c229
quantrinh123 <quantrinh29092003@gmail.com>
2024-08-29 11:59:02 +0700

    update source base

--- src/main/java/com/example/sourcebase/controller/UserRestController.java
+++ src/main/java/com/example/sourcebase/controller/UserRestController.java
@@ -9,13 +9,11 @@ import com.example.sourcebase.util.ResponseData;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
@@ -23,6 +21,7 @@ import java.text.SimpleDateFormat;
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
 public class UserRestController {
     IUserService userService;
+
     @PostMapping
     public ResponseData<?> register(@RequestBody RegisterReqDTO registerReqDTO) {
 //        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
@@ -35,6 +34,32 @@ public class UserRestController {
                 .code(ErrorCode.CREATED.getCode())
                 .message(ErrorCode.CREATED.getMessage())
                 .status(ErrorCode.CREATED.getHttpStatus().name())
+                .build();
+    }
+    @GetMapping("/{id}")
+    public ResponseData<?> getUserById(@PathVariable Long id) {
+        return ResponseData.builder()
+                .status(ErrorCode.GET_SUCCESSFUL.getHttpStatus().name())
+                .code(ErrorCode.GET_SUCCESSFUL.getCode())
+                .message(ErrorCode.GET_SUCCESSFUL.getMessage())
+                .data(userService.getUserById(id))
+                .build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseData<?> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseData.builder()
+                .code(ErrorCode.DELETE_SUCCESSFUL.getCode())
+                .message(ErrorCode.DELETE_SUCCESSFUL.getMessage())
+                .message("Delete user success")
+                .build();
+    }
+    @PatchMapping("/{id}")
+    public ResponseData<?> updateUser(@PathVariable Long id, @RequestBody RegisterReqDTO request) {
+        return ResponseData.builder()
+                .code(ErrorCode.UPDATE_SUCCESSFUL.getCode())
+                .message(ErrorCode.UPDATE_SUCCESSFUL.getMessage())
+                .data(userService.updateUser(id, request))
                 .build();
     }
 }
