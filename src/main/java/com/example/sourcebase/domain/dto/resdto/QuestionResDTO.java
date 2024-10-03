4a1bfc65858670b692aaf150fd34124c8bc55309
daidev07 <hoxuandai02@gmail.com>
2024-10-03 09:18:34 +0700

    feat: fetch questions by criteriaId

---
+++ src/main/java/com/example/sourcebase/domain/dto/resdto/QuestionResDTO.java
@@ -0,0 +1,18 @@
+package com.example.sourcebase.domain.dto.resdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class QuestionResDTO {
+    Long id;
+    String title;
+    int point;
+}
