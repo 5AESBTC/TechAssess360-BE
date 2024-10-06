d3a522b698e49c70de59a1a0e0725f357795cd7f
Minh Quang <hcmquang2111@gmail.com>
2024-10-06 12:21:36 +0700

    api add assess

---
+++ src/main/java/com/example/sourcebase/controller/AssessRestController.java
@@ -0,0 +1,33 @@
+package com.example.sourcebase.controller;
+
+import com.example.sourcebase.domain.dto.reqdto.AssessReqDTO;
+import com.example.sourcebase.service.IAssessService;
+import com.example.sourcebase.util.ResponseData;
+import com.example.sourcebase.util.SuccessCode;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/assess")
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AssessRestController {
+    IAssessService assessService;
+    @PostMapping("/save-assess")
+    public ResponseEntity<ResponseData<?>> saveAssess(@RequestBody AssessReqDTO assessReqDto) {
+        return ResponseEntity.ok(
+                ResponseData.builder()
+                        .code(SuccessCode.CREATED.getCode())
+                        .message(SuccessCode.CREATED.getMessage())
+                        .data(assessService.updateAssess(assessReqDto))
+                        .build()
+        );
+    }
+
+}
