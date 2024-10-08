7b6e2cdde3130a462f96dda272850ab2c9d065ee
quantrinh123 <quantrinh29092003@gmail.com>
2024-10-08 08:15:08 +0700

    uploadfile

---
+++ src/main/java/com/example/sourcebase/domain/dto/resdto/FileInfoResDTO.java
@@ -0,0 +1,15 @@
+package com.example.sourcebase.domain.dto.resdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileInfoResDTO {
+    private String publicId;
+    private String url;
+}
